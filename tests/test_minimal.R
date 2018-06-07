86bb40129bc036cb1512bba650c4f993bc7836d2
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2018-06-07 17:59:14 -0500

    port the python code of fit_ab to Rcpp

--- tests/test_minimal.R
+++ tests/test_minimal.R
@@ -1,4 +1,18 @@
-quant("/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/Human_CRISPR_Evers.fasta", 
-      c("/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R1.fastq",
-        "/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R2.fastq",
-        "/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R2.fastq"))
+library(tidyverse)
+FASTA <- system.file("extdata", "small_sample.fasta", package = "CRISPRExpress")
+FASTQ <- c()
+for(g in c("Base", "High", "Low")) {
+  for(i in 1:2) {
+    FASTQ <- c(FASTQ, system.file("extdata", sprintf("%s%d.fastq", g, i), package="CRISPRExpress"))
+  }
+}
+
+rep.row <- function(x,n) matrix(rep(x,each=n),nrow=n)
+
+lst.quant <- quant(FASTA, FASTQ)
+
+fit_ab(lst.quant$count, 
+       lst.quant$count %>% 
+         colSums %>% 
+         rep.row( nrow(lst.quant$count)))
+

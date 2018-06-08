0667f1e22f4fe45da1827ebcd20097d1b50a1faf
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2018-06-08 04:09:49 -0500

    convert fit_ab code to matrix version

--- tests/test_minimal.R
+++ tests/test_minimal.R
@@ -1,4 +1,18 @@
-quant("/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/Human_CRISPR_Evers.fasta", 
-      c("/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R1.fastq",
-        "/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R2.fastq",
-        "/Users/hwan/Downloads/CRISPR.RT112 - Evers et al. Nature Biotech 2016/RT112_T0_R2.fastq"))
+FASTA <- system.file("extdata", "small_sample.fasta", package = "CRISPRExpress")
+FASTQ <- c()
+for(g in c("Low", "High", "Base")) {
+  for(i in 1:2) {
+    FASTQ <- c(FASTQ, system.file("extdata", 
+                                  sprintf("%s%d.fastq", g, i), 
+                                  package = "CRISPRExpress"))
+  }
+}
+library(tidyverse)
+sample_quant_output <- quant(FASTA, FASTQ)
+rep.row<-function(x,n) matrix(rep(x,each=n),nrow=n)
+xvec <- sample_quant_output$count 
+xvec <- matrix(sample(1:10000, 1e5*10, replace=T), ncol=10)
+xvec[1,] <- 0
+xvec
+nvec <- xvec %>% colSums() %>% rep.row(nrow(xvec))
+as.data.frame(fit_ab(xvec, nvec))
